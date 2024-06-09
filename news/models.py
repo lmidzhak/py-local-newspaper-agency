29ee5223cd8d96e729fd3ed589e66565f4dd24bd
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-06-09 18:11:07 +0300

    Create project models, make migrations to db

--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}: {self.username}"
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    topics = models.ManyToManyField(Topic, related_name="articles")
+    publishers = models.ManyToManyField(Redactor, related_name="articles")
+
+    def __str__(self):
+        return self.title
