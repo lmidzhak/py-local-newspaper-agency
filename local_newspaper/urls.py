8e00a67900dbca26a4c9938f01ecca3ea5c7fd89
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-07-04 23:53:47 +0300

    add views, urls

--- local_newspaper/urls.py
+++ local_newspaper/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -22,4 +23,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("news.urls", namespace="news")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
