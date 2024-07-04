2272051ae7d0b59b2b49c535b4df449e8e3d1696
Liubomyr Midzhak <lmidzhak@gmail.com>
2024-07-04 23:33:17 +0300

    change NewspaperEdition field type in models

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class Topic(models.Model):
@@ -11,31 +12,54 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ["name"]
+
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(blank=True, null=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+        ordering = ["username"]
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}: {self.username}"
+
+    def get_absolute_url(self):
+        return reverse("news:redactor-detail", kwargs={"pk": self.pk})
 
 
 class Article(models.Model):
     title = models.CharField(max_length=255, unique=True)
     content = models.TextField()
     topics = models.ManyToManyField(Topic, related_name="articles")
-    publishers = models.ManyToManyField(Redactor, related_name="articles")
+    redactors = models.ManyToManyField(Redactor, related_name="articles")
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("news:article-detail", kwargs={"pk": self.pk})
 
 
 class NewspaperEdition(models.Model):
     edition = models.IntegerField(default=1)
     published_date = models.DateField(auto_now=True)
     articles = models.ManyToManyField(Article, related_name="editions")
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        ordering = ["edition"]
 
     def __str__(self):
-        return f"Local newspaper edition number {self.edition}, published {self.published_date}"
+        return (
+            f"Local newspaper edition number {self.edition},"
+            f" published {self.published_date}"
+        )
+
+    def get_absolute_url(self):
+        return reverse("news:newspaper-edition-detail", kwargs={"pk": self.pk})
